ab1328379fc602875891915ebfaef2684c01187d
Oksana Marusich <oksana.m229@gmail.com>
2024-04-13 20:11:21 +0300

    add

--- src/components/SwiperExampleS/SwiperExampleS.jsx
+++ src/components/SwiperExampleS/SwiperExampleS.jsx
@@ -23,6 +23,20 @@ export const SwiperExampleS = ({ pictures }) => {
                   nextEl: '.swiper-button-next',
                   prevEl:'.swiper-button-prev',
                   
+              }}
+              breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
         }}
         // pagination={{ clickable: true }}
         // scrollbar={{ draggable: true }}
